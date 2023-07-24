04801844bea2651706e42e0886b0eed9aa38a60c
dasbiplob <biplob90.d@git.com>
2023-07-24 13:40:11 +0300

    SpringBoot Video Streaming Reactive Programming

---
+++ src/main/java/com/example/SpringReactiveVideoStreaming/SpringReactiveVideoStreamingApplication.java
@@ -0,0 +1,29 @@
+package com.example.SpringReactiveVideoStreaming;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class SpringReactiveVideoStreamingApplication {
+
+	@Autowired
+	private StreamingService streamingService;
+	@GetMapping(value = "video/{title}", produces = "video/mp4")
+	public Mono<Resource> getVideos(@PathVariable String title, @RequestHeader("Range") String range){
+
+		return streamingService.getVideo(title);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveVideoStreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringReactiveVideoStreaming/SpringReactiveVideoStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringReactiveVideoStreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveVideoStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
